5a55a552932c2bba949275e91393544979f54999
Kevin Locke <kevin@kevinlocke.name>
2019-11-15 10:47:46 -0700

    test: add tests for TitleCapitalizer
    These tests were collected from Aristotle Pagaltzis' TitleCase.pl and
    David Gouch's to-title-case along with a few additions that I came up
    with during development.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ NLCaseConvert.UnitTests/TitleCapitalizerTests.cs
@@ -0,0 +1,27 @@
+// <copyright file="TitleCapitalizerTests.cs" company="Kevin Locke">
+// Copyright 2019 Kevin Locke.  All rights reserved.
+// </copyright>
+
+namespace NLCaseConvert.UnitTests
+{
+    using System.Globalization;
+
+    using Xunit;
+
+    public static class TitleCapitalizerTests
+    {
+        private static readonly TitleCapitalizer TitleCapitalizer =
+            new TitleCapitalizer.Builder(CultureInfo.InvariantCulture)
+            .Build();
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData(" ", " ")]
+        [MemberData(nameof(TestDataFile.ReadAll), "Titles.txt", MemberType=typeof(TestDataFile))]
+        public static void CapitalizesInvariantCorrectly(string? input, string? expected)
+        {
+            Assert.Equal(expected, TitleCapitalizer.Transform(input));
+        }
+    }
+}
