3350927e6ba849f832c547cec032bd427b026fa6
Kevin Locke <kevin@kevinlocke.name>
2019-11-15 10:42:56 -0700

    test: create TestDataFile
    TestDataFile defines a simple format for test data similar to the format
    used by Aristotle Pagaltzis's tests for John Gruber's Title Case, with
    the following additions:
    
    * Ignoring blank and comment lines
    * Supporting C# escape sequences
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ NLCaseConvert.UnitTests/TestDataFile.cs
@@ -0,0 +1,133 @@
+// <copyright file="TestDataFile.cs" company="Kevin Locke">
+// Copyright 2019 Kevin Locke.  All rights reserved.
+// </copyright>
+
+namespace NLCaseConvert.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Utility functions for dealing with test data files.
+    ///
+    /// A test data file is a UTF-8-encoded plain text file, stored in the
+    /// <c>Test_Data</c> directory, which contains pairs of lines with the
+    /// input first and correct capitalization of the input second.
+    ///
+    /// Lines may contain
+    /// <see href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#string-literals">escape
+    /// sequences as defined for C# 6 regular string literals</see>.  A
+    /// backslash followed by anything other than a valid escape sequence is
+    /// invalid.
+    ///
+    /// Blank lines and comments (lines where the first non-whitespace
+    /// character is <c>#</c>) are ignored.
+    /// </summary>
+    public static class TestDataFile
+    {
+        private static readonly Regex PossibleEscapeRegex =
+            new Regex(@"\\([Uu][0-9a-fA-F]{8}|[Uu][0-9a-fA-F]{4}|x[0-9a-fA-F]{1,4}|.)?");
+
+        public static IEnumerable<string> ReadAllLines(string path)
+        {
+            string fullPath = Path.Join(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Test_Data",
+                path);
+            using var reader = new StreamReader(fullPath, Encoding.UTF8);
+            for (string? line = reader.ReadLine();
+                line != null;
+                line = reader.ReadLine())
+            {
+                if (IsLineIgnored(line))
+                {
+                    continue;
+                }
+
+                yield return Unescape(line);
+            }
+        }
+
+        public static IEnumerable<object[]> ReadAll(string path)
+        {
+            using var lines = ReadAllLines(path).GetEnumerator();
+            while (lines.MoveNext())
+            {
+                string input = lines.Current;
+
+                if (!lines.MoveNext())
+                {
+                    throw new FormatException("Unpaired last line");
+                }
+
+                yield return new[] { input, lines.Current };
+
+                // Ensure tests which include \n also work with \r\n
+                string inputCrLf =
+                    input.Replace("\n", "\r\n", StringComparison.Ordinal);
+                if (!object.ReferenceEquals(input, inputCrLf))
+                {
+                    string expectedCrLf =
+                        lines.Current.Replace("\n", "\r\n", StringComparison.Ordinal);
+                    yield return new[] { inputCrLf, expectedCrLf };
+                }
+            }
+        }
+
+        private static bool IsLineIgnored(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (!char.IsWhiteSpace(line[i]))
+                {
+                    return line[i] == '#';
+                }
+            }
+
+            return true;
+        }
+
+        private static string Unescape(string input)
+        {
+            return PossibleEscapeRegex.Replace(input, Unescape);
+        }
+
+        private static string Unescape(Match match)
+        {
+            string escapeStr = match.Groups[1].Value;
+            char escapeChar = escapeStr.Length > 0 ? escapeStr[0] : '\0';
+            switch (escapeChar)
+            {
+                case '\'': return "'";
+                case '"': return "\"";
+                case '\\': return "\\";
+                case '0': return "\0";
+                case 'a': return "\a";
+                case 'b': return "\b";
+                case 'f': return "\f";
+                case 'n': return "\n";
+                case 'r': return "\r";
+                case 't': return "\t";
+                case 'v': return "\v";
+                case 'u':
+                case 'x':
+                    if (escapeStr.Length > 1)
+                    {
+                        int code = int.Parse(
+                            escapeStr[1..],
+                            NumberStyles.HexNumber,
+                            CultureInfo.InvariantCulture);
+                        return char.ConvertFromUtf32(code);
+                    }
+
+                    break;
+            }
+
+            throw new FormatException("Invalid escape sequence \\" + escapeStr);
+        }
+    }
+}
